bd9b6ca4ab51ff7ffb4ab0b597136a8d7a27e79d
brushscape <void.mordant@gmail.com>
2023-03-23 15:17:54 -0500

    fixed cvs load so pulls from github

--- src/process_CSV.js
+++ src/process_CSV.js
@@ -1,34 +1,26 @@
 /* jshint esversion: 8 */
 
-var filePath = 'files/Pemtara_Eng_Dictionary.csv';
-var testPath = 'files/test.csv';
+//var localFilePath = 'files/Pemtara_Eng_Dictionary.csv';
+var filePath = 'https://raw.githubusercontent.com/brushscape/Pemtara-Dictionary/main/files/Pemtara_Eng_Dictionary.csv';
 
 function readCSVFile(){
-  $.get(filePath, function (data) {
-        //var file = document.getElementById("csvFile").files[0];
+  $.get(filePath, function (csvdata) {
+      var rowData = csvdata.split('\n');
+      var colHeaders = rowData[0].split(',');
 
-        var reader = new FileReader();
-        reader.readAsText(data);
-
-        reader.onload = function(event) {
-          var csvdata = event.target.result;
-          var rowData = csvdata.split('\n');
-          var colHeaders = rowData[0].split(',');
-
-          for (var row = 1; row < rowData.length; row++) {
-            var rowColData = rowData[row].split(',');
-            var rowObj = {};
-            for (var col = 0; col < rowColData.length-1; col++) {
-              if(rowColData[col].search('/') != -1){
-                rowObj[colHeaders[col].replace(/\s/g, "")] = rowColData[col].split('/');
-              }else{
-                rowObj[colHeaders[col].replace(/\s/g, "")] = rowColData[col];
-              }
-            }
-            pemtaraDict.push(rowObj);
+      for (var row = 1; row < rowData.length; row++) {
+        var rowColData = rowData[row].split(',');
+        var rowObj = {};
+        for (var col = 0; col < rowColData.length-1; col++) {
+          if(rowColData[col].search('/') != -1){
+            rowObj[colHeaders[col].replace(/\s/g, "")] = rowColData[col].split('/');
+          }else{
+            rowObj[colHeaders[col].replace(/\s/g, "")] = rowColData[col];
           }
-        };
-    });
+        }
+        pemtaraDict.push(rowObj);
+      }
+  });
 
   console.log('done');
 }
