c6cd298e8964b56c3aa236ecb123391362caeee3
brushscape <void.mordant@gmail.com>
2023-03-31 21:10:24 -0500

    implemented submenu (she's ugly but she works)

--- src/page_func.js
+++ src/page_func.js
@@ -1,6 +1,7 @@
 /* jshint esversion: 8 */
 
 var currPage = 'translator';
+var quickPage = 'partsOfSpeech';
 
 function load(){
   readCSVFile();
@@ -15,21 +16,33 @@ function setupDictionarySearch(){
 
 function setupMenuButtons(){
   document.getElementById('translatorPg').style.borderColor = 'var(--white)';
+  document.getElementById('quickMenu').style.display = 'none';
   $('.menuItem').click(function(event) {
       var page;
       var str = this.innerHTML.substring(0,4).toLowerCase();
+      var quick = false;
       switch(str){
         case 'tran':
           page = 'translator';
           break;
+        case 'quic':
+          page = quickPage;
+          quick = true;
+          break;
         case 'part':
           page = 'partsOfSpeech';
+          quickPage = 'partsOfSpeech';
+          quick = true;
           break;
         case 'comm':
           page = 'prefixSuffix';
+          quickPage = 'prefixSuffix';
+          quick = true;
           break;
         case 'hono':
           page = 'honorifics';
+          quickPage = 'honorifics';
+          quick = true;
           break;
         case 'link':
           page = 'links';
@@ -40,6 +53,16 @@ function setupMenuButtons(){
         default:
           page = '';
       }
+
+      if(!quick){
+        document.getElementById('quickMenu').style.display = 'none';
+        document.getElementById('quickStartPg').style.borderColor = 'var(--dark)';
+      }else{
+        document.getElementById('quickStartPg').style.borderColor = 'var(--white)';
+        document.getElementById('quickMenu').style.display = 'flex';
+        document.getElementById(quickPage+'Pg').style.borderColor = 'var(--white)';
+      }
+
       if(currPage != page){
         document.getElementById(currPage).style.display = 'none';
         document.getElementById(page).style.display = 'flex';
