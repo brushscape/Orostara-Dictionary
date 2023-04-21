8044976393a78c3b6d771b2fd32270b459a60773
brushscape <void.mordant@gmail.com>
2023-04-20 23:13:29 -0500

    bug fix

--- src/dict_process_pem_word.js
+++ src/dict_process_pem_word.js
@@ -1,69 +1,69 @@
 /* jshint esversion: 8 */
 
-function pemClick(searchedWord){
+function pemClick(searchedWord) {
   clearPage();
-  var lastChar = searchedWord.charAt(searchedWord.length-1);
+  var lastChar = searchedWord.charAt(searchedWord.length - 1);
   var endList = ['a', 'e', 'o', 'i', 'u'];
-  var specialCases = ['naki','amki','ki','ha','ye','je','na'];
+  var specialCases = ['naki', 'amki', 'ki', 'ha', 'na', 'a'];
   var otherWord = '';
-  if(specialCases.indexOf(searchedWord) == -1 && endList.indexOf(lastChar) != -1){
-    if(properNouns.indexOf(searchedWord) == -1 ){
+  if (specialCases.indexOf(searchedWord) == -1 && endList.indexOf(lastChar) != -1) {
+    if (properNouns.indexOf(searchedWord) == -1) {
       otherWord = searchedWord;
     }
-    searchedWord = searchedWord.substring(0, searchedWord.length-1);
+    searchedWord = searchedWord.substring(0, searchedWord.length - 1);
 
   }
 
   var found = pemEntry(searchedWord, otherWord);
-  if(found == 1){
-    incMemory('Pemtara',searchedWord);
-  }else if(found == 2){
-    incMemory('Pemtara',otherWord);
-  }else if(found == 3){ // incase they search a word like 'kanan' which could be 'kanan' without an ending or 'kan' with an ending
-    incMemory('Pemtara2',[searchedWord,otherWord]);
+  if (found == 1) {
+    incMemory('Pemtara', searchedWord);
+  } else if (found == 2) {
+    incMemory('Pemtara', otherWord);
+  } else if (found == 3) { // incase they search a word like 'kanan' which could be 'kanan' without an ending or 'kan' with an ending
+    incMemory('Pemtara2', [searchedWord, otherWord]);
   }
 }
 
-function pemEntry(searchedWord, otherWord){
+function pemEntry(searchedWord, otherWord) {
   var entry1 = searchPemtara(searchedWord);
   var entry2 = [];
-  if(otherWord != ''){
+  if (otherWord != '') {
     entry2 = searchPemtara(otherWord);
   }
-  if(entry1.length == 0 && entry2.length == 0){
-    document.getElementById('notFoundPem').style.display='flex';
+  if (entry1.length == 0 && entry2.length == 0) {
+    document.getElementById('notFoundPem').style.display = 'flex';
     return 0;
-  }else{
-    var full = pushArray(entry1,entry2);
-    displayEntryArry(full,'');
-    if(entry1.length != 0){
-      if(entry2.length !=0){
+  } else {
+    var full = pushArray(entry1, entry2);
+    displayEntryArry(full, '');
+    if (entry1.length != 0) {
+      if (entry2.length != 0) {
         return 3;
-      }else{
+      } else {
         return 1;
       }
-    }else{
+    } else {
       return 2;
     }
   }
 }
 
-function searchPemtara(word1){
+function searchPemtara(word1) {
   var entryArr = [];
   var word = word1.toLowerCase();
   //check every word in case there's more than one (rare but not unheardof)
-  for(var i=0; i<pemtaraDict.length; i++){
+  for (var i = 0; i < pemtaraDict.length; i++) {
     var checkWord = pemtaraDict[i].Pemtara.toLowerCase();
-    if(checkWord == word){
+    if (checkWord == word) {
       entryArr.push(pemtaraDict[i]);
     }
   }
   return entryArr;
 }
 
-function pushArray(base, add){
+function pushArray(base, add) {
   var newArr = base;
-  for(var i=0;i<add.length;i++){
+  for (var i = 0; i < add.length; i++) {
     newArr.push(add[i]);
   }
   return newArr;
