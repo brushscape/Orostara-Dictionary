25fdbd571915a8546ddc33c1418d42ecf8ecea0e
brushscape <void.mordant@gmail.com>
2023-12-16 20:54:01 -0600

    dict updates and added common phrases to the quickstart guide

--- src/page_func.js
+++ src/page_func.js
@@ -1,7 +1,7 @@
 /* jshint esversion: 8 */
 
-var currPage = 'translator';
-var quickPage = 'partsOfSpeech';
+var currPage = "translator";
+var quickPage = "partsOfSpeech";
 
 function load() {
   readCSVFile();
@@ -15,74 +15,80 @@ function setupDictionarySearch() {
 }
 
 function setupMenuButtons() {
-  document.getElementById('quickMenu').style.display = 'none';
-  $('.menuItem').click(function(event) {
+  document.getElementById("quickMenu").style.display = "none";
+  $(".menuItem").click(function (event) {
     var page;
     var str = this.innerHTML.substring(0, 4).toLowerCase();
     var quick = false;
     switch (str) {
-      case 'tran':
-        page = 'translator';
+      case "tran":
+        page = "translator";
         break;
-      case 'quic':
+      case "quic":
         page = quickPage;
         quick = true;
         break;
-      case 'part':
-        page = 'partsOfSpeech';
-        quickPage = 'partsOfSpeech';
+      case "part":
+        page = "partsOfSpeech";
+        quickPage = "partsOfSpeech";
         quick = true;
         break;
-      case 'comm':
-        page = 'prefixSuffix';
-        quickPage = 'prefixSuffix';
+      case "pref":
+        page = "prefixSuffix";
+        quickPage = "prefixSuffix";
         quick = true;
         break;
-      case 'hono':
-        page = 'honorifics';
-        quickPage = 'honorifics';
+      case "hono":
+        page = "honorifics";
+        quickPage = "honorifics";
         quick = true;
         break;
-      case 'link':
-        page = 'links';
+      case "comm":
+        page = "phrases";
+        quickPage = "phrases";
+        quick = true;
         break;
-      case 'full':
-        page = 'fullDict';
+      case "link":
+        page = "links";
         break;
-      case 'cate':
-        page = 'category';
+      case "full":
+        page = "fullDict";
+        break;
+      case "cate":
+        page = "category";
         break;
       default:
-        page = '';
+        page = "";
     }
 
     if (!quick) {
-      document.getElementById('quickMenu').style.display = 'none';
-      document.getElementById('quickStartPg').classList.remove('activeSpecial');
+      document.getElementById("quickMenu").style.display = "none";
+      document.getElementById("quickStartPg").classList.remove("activeSpecial");
     } else {
-      document.getElementById('quickStartPg').classList.add('activeSpecial');
-      document.getElementById('quickMenu').style.display = 'flex';
+      document.getElementById("quickStartPg").classList.add("activeSpecial");
+      document.getElementById("quickMenu").style.display = "flex";
     }
 
     if (currPage != page) {
-      document.getElementById(currPage).style.display = 'none';
-      document.getElementById(page).style.display = 'flex';
+      document.getElementById(currPage).style.display = "none";
+      document.getElementById(page).style.display = "flex";
 
-      var tab = document.getElementById(currPage + 'Pg');
-      if (tab.classList.contains('quickItem')) { //reset colors
-        tab.classList.remove('quickActive');
+      var tab = document.getElementById(currPage + "Pg");
+      if (tab.classList.contains("quickItem")) {
+        //reset colors
+        tab.classList.remove("quickActive");
       } else {
-        tab.classList.remove('active');
+        tab.classList.remove("active");
       }
 
-      tab = document.getElementById(page + 'Pg');
-      if (tab.classList.contains('quickItem')) { //select colors
-        tab.classList.add('quickActive');
+      tab = document.getElementById(page + "Pg");
+      if (tab.classList.contains("quickItem")) {
+        //select colors
+        tab.classList.add("quickActive");
       } else {
-        tab.classList.add('active');
+        tab.classList.add("active");
       }
       currPage = page;
     }
   });
-
 }
