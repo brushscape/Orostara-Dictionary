2a96e209b422322d253497b826570f8c734660fd
brushscape <void.mordant@gmail.com>
2024-01-05 13:40:03 -0600

    dict updates and fix slang showing up before basic words in search

--- src/dict_process_eng_word.js
+++ src/dict_process_eng_word.js
@@ -4,18 +4,18 @@ function engClick(searchedWord) {
   clearPage();
   var found = engEntry(searchedWord);
   if (found) {
-    incMemory('English', searchedWord);
+    incMemory("English", searchedWord);
   }
 }
 
 function engEntry(searchedWord) {
   if (searchedWord.length == 0) {
-    document.getElementById('notFoundEng').style.display = 'flex';
+    document.getElementById("notFoundEng").style.display = "flex";
     return false;
   }
   var entryArr = searchEnglish(searchedWord);
   if (entryArr.length == 0) {
-    document.getElementById('notFoundEng').style.display = 'flex';
+    document.getElementById("notFoundEng").style.display = "flex";
     return false;
   } else {
     displayEntryArry(entryArr, searchedWord);
@@ -31,15 +31,23 @@ function searchEnglish(word) {
   //go through every dictionary entry
 
   for (var i = 0; i < orosDict.length; i++) {
-    var entriesToCheck = [orosDict[i].Nouns, orosDict[i].Verbs, orosDict[i].Adjectives, orosDict[i].Adverbs, orosDict[i].Other];
+    var entriesToCheck = [
+      orosDict[i].Nouns,
+      orosDict[i].Verbs,
+      orosDict[i].Adjectives,
+      orosDict[i].Adverbs,
+      orosDict[i].Other,
+    ];
 
     var add = false; //word is contained in entry
     var perfect = false; // word is a perfect match to an entry
-    for (var j = 0; j < entriesToCheck.length; j++) { //check ALL of the entries, even if found, in case there is a perfect match so that can be displayed first
+    for (var j = 0; j < entriesToCheck.length; j++) {
+      //check ALL of the entries, even if found, in case there is a perfect match so that can be displayed first
       //search each entry option
       var array = entriesToCheck[j];
       if (!Array.isArray(array)) {
-        if (array == '') { //skip if entry is empty
+        if (array == "") {
+          //skip if entry is empty
           continue;
         }
         //make an array if only one entry
@@ -51,14 +59,22 @@ function searchEnglish(word) {
     }
 
     if (add) {
-      if (orosDict[i].RootLanguage != 'Orostara') { //basic word
+      var isBasic = true;
+      if (Array.isArray(orosDict[i].Type)) {
+        isBasic = [i].indexOf("basic");
+      } else {
+        isBasic = orosDict[i].Type == "basic";
+      }
+      if (isBasic) {
+        //basic word
         // add to the front of the array
         if (perfect) {
           perfectArr.unshift(orosDict[i]);
         } else {
           nearArr.unshift(orosDict[i]);
         }
-      } else { //constructed word
+      } else {
+        //constructed word
         //add to the back of the array
         if (perfect) {
           perfectArr.push(orosDict[i]);
@@ -77,20 +93,19 @@ function arrayHasWord(arr, word1) {
   var word = word1.toLowerCase();
   //search through array
   for (var j = 0; j < arr.length; j++) {
-
     //cleanup
     var check = arr[j];
-    if (check.charAt(0) == '(') {
+    if (check.charAt(0) == "(") {
       var index = 1;
       //find end of paranthetical
-      while (check.charAt(index) != ')' && index < check.length) {
+      while (check.charAt(index) != ")" && index < check.length) {
         index++;
       }
 
       var newCheck = check.substring(index + 1);
 
       //take away space between parathentical and word if there
-      if (newCheck.charAt(0) == ' ' && newCheck.length > 1) {
+      if (newCheck.charAt(0) == " " && newCheck.length > 1) {
         check = newCheck.substring(1);
       } else {
         check = newCheck;
@@ -103,9 +118,9 @@ function arrayHasWord(arr, word1) {
     }
 
     //if array element is more than one word
-    if (check.indexOf(' ') != -1) {
+    if (check.indexOf(" ") != -1) {
       //make array with both words
-      var multiDef = check.split(' ');
+      var multiDef = check.split(" ");
       //recurse
       if (arrayHasWord(multiDef, word)[0]) {
         return [true, false];
