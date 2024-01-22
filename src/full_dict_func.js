2df2cc0ed0d6bde8c6ca58680021d6f146562588
brushscape <void.mordant@gmail.com>
2024-01-22 17:54:55 -0600

    realized I should fix the dropdown on the full dict too

--- src/full_dict_func.js
+++ src/full_dict_func.js
@@ -1,14 +1,12 @@
-var prev = 'full';
+var prev = "full";
 
 function onDropHover() {
-  $('.menuItem').hover(function(event) {
-
-  });
+  $(".menuItem").hover(function (event) {});
 }
 
 function fillTable() {
-  var containerEl = document.getElementById('fullDictTable');
-  var baseEl = document.getElementById('dictDataRow');
+  var containerEl = document.getElementById("fullDictTable");
+  var baseEl = document.getElementById("dictDataRow");
 
   for (var i = 0; i < orosDict.length; i++) {
     var rowObj = orosDict[i];
@@ -16,20 +14,18 @@ function fillTable() {
     var wordTypes;
     if (Array.isArray(rowObj.Type)) {
       wordTypes = rowObj.Type;
-      if (rowObj.Type.indexOf('basic') == -1) {
-        wordTypes.push('construct');
+      if (rowObj.Type.indexOf("basic") == -1) {
+        wordTypes.push("construct");
       }
-    }
-    else if (rowObj.Type == '') {
-      wordTypes = ['construct'];
-    }
-    else {
+    } else if (rowObj.Type == "") {
+      wordTypes = ["construct"];
+    } else {
       wordTypes = [rowObj.Type];
     }
 
     var row = baseEl.cloneNode(true);
     row.id = rowObj.Orostara;
-    row.classList.add(...wordTypes)
+    row.classList.add(...wordTypes);
     row.children[0].innerHTML = rowObj.Orostara;
     row.children[1].innerHTML = simpleListDisplay(rowObj.Nouns);
     row.children[2].innerHTML = simpleListDisplay(rowObj.Verbs);
@@ -37,23 +33,22 @@ function fillTable() {
     row.children[4].innerHTML = simpleListDisplay(rowObj.Adverbs);
     row.children[5].innerHTML = simpleListDisplay(rowObj.Other);
     row.children[6].innerHTML = simpleListDisplay(rowObj.Notes);
-    row.style.display = 'inlineBlock';
+    row.style.display = "inlineBlock";
     containerEl.appendChild(row);
   }
 
-  baseEl.style.display = 'none';
-  document.getElementById('numWords').innerHTML = orosDict.length;
-
+  baseEl.style.display = "none";
+  document.getElementById("numWords").innerHTML = orosDict.length;
 }
 
 function simpleListDisplay(array) {
   if (!Array.isArray(array)) {
     return array.toLowerCase();
   }
-  var string = '';
+  var string = "";
   for (i = 0; i < array.length; i++) {
     if (i < array.length - 1) {
-      string += array[i] + ', ';
+      string += array[i] + ", ";
     } else {
       string += array[i];
     }
@@ -61,61 +56,60 @@ function simpleListDisplay(array) {
   return string.toLowerCase();
 }
 
-
 function showOnly(type) {
   if (type == prev) {
     return;
   }
 
-  var fullTable = document.getElementById('fullDictTable');
+  var fullTable = document.getElementById("fullDictTable");
   var counter = 0;
 
   for (var i = 1; i < fullTable.children.length; i++) {
     var el = fullTable.children[i];
-    if (el.classList.contains(type) || type == 'full') {
-      el.style.display = 'table-row';
+    if (el.classList.contains(type) || type == "full") {
+      el.style.display = "table-row";
       if (counter % 2 == 1) {
-        el.style.backgroundColor = 'var(--lightlight2)';
+        el.style.backgroundColor = "var(--lightlight2)";
       } else {
-        el.style.backgroundColor = 'white';
+        el.style.backgroundColor = "white";
       }
       counter++;
     } else {
-      el.style.display = 'none';
+      el.style.display = "none";
     }
   }
 
-  var but = document.getElementById('dropbtn');
-  var string = '';
+  var but = document.getElementById("dropbtn");
+  var string = "";
   switch (type) {
-    case 'basic':
-      string += 'Basic Words';
+    case "basic":
+      string += "Basic Words";
       break;
-    case 'construct':
-      string += 'Constructed Words';
+    case "construct":
+      string += "Constructed Words";
       break;
-    case 'swear':
-      string += 'Swear Words';
+    case "swear":
+      string += "Swear Words";
       break;
-    case 'slang':
-      string += 'Slang Words';
+    case "slang":
+      string += "Slang Words";
       break;
-    case 'full':
-      string += 'Full Dictionary';
+    case "full":
+      string += "Full Dictionary";
       break;
-    case 'proper':
-      string += 'Proper Nouns';
+    case "proper":
+      string += "Proper Nouns";
       break;
     default:
-      string += 'Help';
+      string += "Help";
       break;
   }
   string += "&nbsp;<span style='font-size: var(--midSize);'>&#9660</span>"; // arrow
   but.innerHTML = string;
 
-  document.getElementById('drop' + type).style.display = 'none';
-  document.getElementById('drop' + prev).style.display = 'block';
+  document.getElementById("drop" + type).setAttribute("disabled", "");
+  document.getElementById("drop" + prev).removeAttribute("disabled");
   prev = type;
 
-  document.getElementById('numWords').innerHTML = counter;
+  document.getElementById("numWords").innerHTML = counter;
 }
